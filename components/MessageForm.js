b9341d3890b87b8e6433f17b291be38d1b8d5fe3
Alviss Raghnall <isiomastewart@gmail.com>
2022-07-03 19:18:47 -0700

    Billy Idol - White Wedding

--- components/MessageForm.js
+++ components/MessageForm.js
@@ -1,8 +1,34 @@
-const MessageForm = () => {
+import { useState } from 'react';
+import { sendMessage, isTyping } from "react-chat-engine";
+
+const MessageForm = (props) => {
+
+    const [value, setValue] = useState('');
+    const { chatId, creds } = props;
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault();
+        const text = value.trim();
+
+        if (text.length > 0) sendMessage(creds, chatId, { text });
+    }
+
+    const handleChange = (ev) => {
+        setValue(ev.target.value);
+        isTyping(props, chatId);
+    }
 
     return (
         <>
-            
+            <form className="message-form" onSubmit={handleSubmit}>
+                <input
+                    className="message-input"
+                    placeholder="Send a message ..."
+                    value={value}
+                    onChange={handleChange}
+                    onSubmit={handleSubmit}
+                />
+            </form>
         </>
     )
 }
