648d7d814fd1a37c46e8714f8c6be3d110872fd3
Alviss Raghnall <isiomastewart@gmail.com>
2023-07-24 19:20:21 +0100

    Mr 2Much - My Wife Say

--- components/GetStarted.js
+++ components/GetStarted.js
@@ -2,16 +2,16 @@ import Link from "next/link";
 
 export default function GetStarted() {
     return (
-        <div class="flex h-screen w-screen items-center justify-center" style="background-image:url(https://img.freepik.com/free-vector/blue-pink-halftone-background_53876-99004.jpg?w=2000)">
-            <div class="relative flex w-[40rem] flex-col justify-center overflow-hidden rounded-lg bg-white/50 py-32">
-                <span class="absolute top-0 h-1 w-1/6 bg-blue-600"></span>
+        <div className="flex h-screen w-screen items-center justify-center" style="background-image:url(https://img.freepik.com/free-vector/blue-pink-halftone-background_53876-99004.jpg?w=2000)">
+            <div className="relative flex w-[40rem] flex-col justify-center overflow-hidden rounded-lg bg-white/50 py-32">
+                <span className="absolute top-0 h-1 w-1/6 bg-blue-600"></span>
 
-                <div class="flex flex-col items-center">
-                    <p class="text-lg font-medium text-gray-600">Hello, random internet user!</p>
-                    <h1 class="text-4xl font-medium text-gray-600">Welcome to Maxi!</h1>
+                <div className="flex flex-col items-center">
+                    <p className="text-lg font-medium text-gray-600">Hello, random internet user!</p>
+                    <h1 className="text-4xl font-medium text-gray-600">Welcome to Maxi!</h1>
 
                     <Link href="/login">
-                        <button class="group mt-10 flex w-40 items-center justify-center rounded-lg bg-blue-700 py-2 text-center font-bold text-white transition">
+                        <button className="group mt-10 flex w-40 items-center justify-center rounded-lg bg-blue-700 py-2 text-center font-bold text-white transition">
                             Sign In
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import React, { useState } from 'react';
 import GetStarted from '../components/GetStarted';
 
 const Loading = () => {
-  
+
 }
 
 export default function Home() {
@@ -20,11 +20,20 @@ export default function Home() {
       console.log("fire 1")
       return (
         <div class="bg-gray-200 w-full min-h-screen flex justify-center items-center">
-          <svg class="w-24 h-24 animate-bounce" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 18.25C15.5 18.25 19.25 16.5 19.25 12C19.25 7.5 15.5 5.75 12 5.75C8.5 5.75 4.75 7.5 4.75 12C4.75 13.0298 4.94639 13.9156 5.29123 14.6693C5.50618 15.1392 5.62675 15.6573 5.53154 16.1651L5.26934 17.5635C5.13974 18.2547 5.74527 18.8603 6.43651 18.7307L9.64388 18.1293C9.896 18.082 10.1545 18.0861 10.4078 18.1263C10.935 18.2099 11.4704 18.25 12 18.25Z"></path>
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M9.5 12C9.5 12.2761 9.27614 12.5 9 12.5C8.72386 12.5 8.5 12.2761 8.5 12C8.5 11.7239 8.72386 11.5 9 11.5C9.27614 11.5 9.5 11.7239 9.5 12Z"></path>
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M12.5 12C12.5 12.2761 12.2761 12.5 12 12.5C11.7239 12.5 11.5 12.2761 11.5 12C11.5 11.7239 11.7239 11.5 12 11.5C12.2761 11.5 12.5 11.7239 12.5 12Z"></path>
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M15.5 12C15.5 12.2761 15.2761 12.5 15 12.5C14.7239 12.5 14.5 12.2761 14.5 12C14.5 11.7239 14.7239 11.5 15 11.5C15.2761 11.5 15.5 11.7239 15.5 12Z"></path>
+          <svg fill="none" className="w-24 h-24 animate-bounce" viewBox="0 0 24 24">
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
+              d="M12 18.25c3.5 0 7.25-1.75 7.25-6.25S15.5 5.75 12 5.75 4.75 7.5 4.75 12c0 1.03.196 1.916.541 2.67.215.47.336.987.24 1.495l-.262 1.399a1 1 0 001.168 1.167l3.207-.602a2.24 2.24 0 01.764-.003c.527.084 1.062.124 1.592.124z"
+            ></path>
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9.5 12a.5.5 0 11-1 0 .5.5 0 011 0zM12.5 12a.5.5 0 11-1 0 .5.5 0 011 0zM15.5 12a.5.5 0 11-1 0 .5.5 0 011 0z"
+            ></path>
           </svg>
         </div>
       );
